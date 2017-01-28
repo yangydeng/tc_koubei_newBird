9c8a57d472f0a5b8d09ababb9cd675b69b463435
yangydeng <yangydeng@163.com>
2017-01-28 17:09:42 +0800

    add calculate_score 2017_01_28

--- dyy/tools/tools.py
+++ dyy/tools/tools.py
@@ -4,6 +4,7 @@ Created on Thu Jan 26 19:28:32 2017
 
 @author: 邓旸旸
 """
+import numpy as np
 
 def gb2312(col):
     col_return = []
@@ -23,3 +24,51 @@ def transform_data(data):
         i=i+1
         
     return data
+
+'''本模块用于将data 转变为np.array的形式，以便获取每一列。eg: data[:,1],data[:,2]...'''
+def np_transform_data(data):
+    data = np.array(data)
+    i = 0
+    for row in data:
+        data[i]=np.array(row)
+        i=i+1
+        
+    return data    
+    
+
+'''本模块用于计算得分，pre：预测的客流，real：实际的客流。两者都应当为DataFrame'''
+def calculate_score(pre,real):
+    if(len(pre)!=len(real)):
+        print 'len(pre)!=len(real)','\n'
+    if(len(pre.columns)!=len(real.columns)):
+        print 'len(pre.columns)!=len(real.columns)','\n'
+        
+    N = len(pre)    #N：商家总数
+    T = len(pre.columns)    
+    print 'N:',N,'\t','T:',T,'\n'
+    
+    n = 0
+    t = 0
+    L=0
+    
+    while(t<T):
+        n=0
+        while(n<N):
+            c_it=pre.ix[n,t]        #c_it：预测的客流量
+            c_git = real.ix[n,t]    #c_git：实际的客流量
+            
+            
+            if(c_it==0 and c_git==0):
+                c_it=1
+                c_git=1
+            
+            L = L+abs((float(c_it)-c_git)/(c_it+c_git))
+            n=n+1
+        t=t+1
+    print L
+
+    
+
+
+
+
