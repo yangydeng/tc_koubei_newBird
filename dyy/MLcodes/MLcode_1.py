28738277d609275c0f3a7e4c231f30ca6cf6a4c9
yangydeng <yangydeng@163.com>
2017-02-10 10:12:18 +0800

    2-10 morining

--- dyy/MLcodes/MLcode_1.py
+++ dyy/MLcodes/MLcode_1.py
@@ -24,33 +24,32 @@ poly = PolynomialFeatures(3,interaction_only=True)
 
 train_x = (pd.merge(weekA,weekB,on='shop_id')).drop('shop_id',axis=1)       #train = weekA + weekB
 train_sum = train_x.sum(axis=1)
-#train_mean = train_x.mean(axis=1)
-train_weekend = ['2016-09-24 00:00:00','2016-09-25 00:00:00','2016-10-15 00:00:00','2016-10-15 00:00:00']
+train_mean = train_x.mean(axis=1)
+train_weekend = ['2016-09-24 00:00:00','2016-09-25 00:00:00','2016-10-15 00:00:00','2016-10-16 00:00:00']
 train_ratio_wk = (train_x[train_weekend]).sum(axis=1)/(train_sum.replace(0,1))
-
 train_open_ratio_wkA = every_shop_open_ratio(start_day=447,end_day=453)
 train_open_ratio_wkB = every_shop_open_ratio(start_day=468,end_day=474)
 train_open_ratio = (train_open_ratio_wkA.open_ratio + train_open_ratio_wkB.open_ratio)/2
 
 train_x = transfrom_Arr_DF(poly.fit_transform(train_x)) #将其中的日期生成多项式
 train_x['sumABCD'] = train_sum    #加入总数
-#train_x['meanABCD'] = train_mean     #加入平均数
+train_x['meanABCD'] = train_mean     #加入平均数
 train_x['ratio_wk'] = train_ratio_wk  #周末占总量的比例
 train_x['open_ratio_ABCD'] = train_open_ratio.values    #加入开业比例
 
 train_y = weekD.drop('shop_id',axis=1)
 
-
+#-----------------------------------------------------------------------------------------------------------------------------------------------------
 test_x = (pd.merge(weekB,weekC,on='shop_id')).drop('shop_id',axis=1)    #test = weekC + weekD
 test_sum = test_x.sum(axis=1)
-#test_mean = test_x.mean(axis=1)
+test_mean = test_x.mean(axis=1)
 test_weekend = ['2016-10-15 00:00:00','2016-10-15 00:00:00','2016-10-22 00:00:00','2016-10-23 00:00:00']
 test_ratio_wk = (test_x[test_weekend]).sum(axis=1)/(test_sum.replace(0,1))
 test_open_ratio = every_shop_open_ratio(start_day=468,end_day=481)
 
 test_x = transfrom_Arr_DF(poly.fit_transform(test_x))
 test_x['sumABCD'] = test_sum
-#test_x['meanABCD'] = test_mean
+test_x['meanABCD'] = test_mean
 test_x['ratio_wk'] = test_ratio_wk
 test_x['open_ratio_ABCD'] = test_open_ratio.open_ratio
 
--- dyy/MLcodes/MLcode_2.py
+++ dyy/MLcodes/MLcode_2.py
@@ -19,24 +19,28 @@ weekC = pd.read_csv('../train/weekABCD/weekC.csv'); weekC.index=weekC.shop_id
 weekD = pd.read_csv('../train/weekABCD/weekD.csv'); weekD.index=weekD.shop_id
 
 poly = PolynomialFeatures(3,interaction_only=True)
-train_x = (pd.merge(weekB,weekC,on='shop_id')).drop('shop_id',axis=1)    #train = weekB + weekC
+train_x = pd.merge(weekA,weekB,on='shop_id')                                       #train = weekA+ weekB + weekC
+train_x = (pd.merge(train_x,weekC,on='shop_id')).drop('shop_id',axis=1)
 train_sum = train_x.sum(axis=1)
-train_open_ratio = every_shop_open_ratio(start_day=468,end_day=481)
-train_weekend = ['2016-10-15 00:00:00','2016-10-15 00:00:00','2016-10-22 00:00:00','2016-10-23 00:00:00']
+train_open_ratio_A = every_shop_open_ratio(start_day=447,end_day=453)
+train_open_ratio_BC = every_shop_open_ratio(start_day=468,end_day=481)
+train_open_ratio = (train_open_ratio_A.open_ratio + train_open_ratio_BC.open_ratio*2)/3
+train_weekend = ['2016-09-24 00:00:00','2016-09-25 00:00:00','2016-10-15 00:00:00','2016-10-16 00:00:00','2016-10-22 00:00:00','2016-10-23 00:00:00']
 train_ratio_wk = (train_x[train_weekend]).sum(axis=1)/(train_sum.replace(0,1))
 
 train_x = transfrom_Arr_DF(poly.fit_transform(train_x))
 train_x['sumABCD'] = train_sum
-train_x['open_ratio'] = train_open_ratio.open_ratio
+train_x['open_ratio'] = train_open_ratio
 train_x['ratio_wk'] = train_ratio_wk
 
 train_y = weekD.drop('shop_id',axis=1)
 
 
-test_x = (pd.merge(weekC,weekD,on='shop_id')).drop('shop_id',axis=1)     #test = weekC + weekD 
+test_x = pd.merge(weekB,weekC,on='shop_id')                #test = weekB + weekC + weekD 
+test_x = (pd.merge(test_x,weekD,on='shop_id')).drop('shop_id',axis=1)
 test_sum = test_x.sum(axis=1)
-test_open_ratio = every_shop_open_ratio(start_day=475)
-test_weekend = ['2016-10-22 00:00:00','2016-10-23 00:00:00','2016-10-29 00:00:00','2016-10-30 00:00:00']
+test_open_ratio = every_shop_open_ratio(start_day=468)
+test_weekend = ['2016-10-15 00:00:00','2016-10-16 00:00:00','2016-10-22 00:00:00','2016-10-23 00:00:00','2016-10-29 00:00:00','2016-10-30 00:00:00']
 test_ratio_wk = (test_x[test_weekend]).sum(axis=1)/(test_sum.replace(0,1))
 
 test_x = transfrom_Arr_DF(poly.fit_transform(test_x))
